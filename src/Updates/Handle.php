3e2643e5e2c4eaa2e381bcff4d2b1713ba923fc0
Azad <AzadPC1324@outlook.com>
2024-04-17 23:14:42 +0330

    .

--- src/Updates/Handle.php
+++ src/Updates/Handle.php
@@ -5,11 +5,13 @@ namespace Lanous\Telegram\Updates;
 class Handle {
     public $chat_id;
     public $first_name;
+    public $last_name;
     public $text;
     public $invite_id=false;
     public function __construct($Update) {
         $this->chat_id = $Update["message"]['chat']['id'] ?? null;
         $this->first_name = $Update["message"]['chat']['first_name'] ?? null;
+        $this->last_name = $Update["message"]['chat']['last_name'] ?? null;
         $this->text = $Update["message"]['text'] ?? null;
         if(explode(" ",$this->text)[0] == "/start" and isset(explode(" ",$this->text)[1])) {
             $this->invite_id = explode(" ",$this->text)[1];
