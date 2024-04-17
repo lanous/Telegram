8ee99ef25b761684869330954fdd36d1a2e42ff3
Azad <AzadPC1324@outlook.com>
2024-04-17 23:17:32 +0330

    .

--- Languages/En/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    "start" => [
-        "normal" => "hello :first_name: your chatid: :chat_id: ",
-        "invite" => "you invited by :first_name_inviter:",
-    ],
-];
--- Languages/Fa/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    "start" => [
-        "normal" => "سلام :first_name: شناسه ی کاربری شما :chat_id: ",
-        "invite" => "شما توسط :first_name_inviter: دعوت شدید!",
-    ],
-];
