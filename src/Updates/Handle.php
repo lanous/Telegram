55bdd772c7a98457d38143f60303882cd16cc5ed
Azad <AzadPC1324@outlook.com>
2024-04-23 18:04:55 +0330

    .

--- src/Updates/Handle.php
+++ src/Updates/Handle.php
@@ -7,12 +7,17 @@ class Handle {
     public $first_name;
     public $last_name;
     public $text;
+    public $cb_data;
+    public $cb_chat_id;
     public $invite_id=false;
     public function __construct($Update) {
         $this->chat_id = $Update["message"]['chat']['id'] ?? null;
         $this->first_name = $Update["message"]['chat']['first_name'] ?? null;
         $this->last_name = $Update["message"]['chat']['last_name'] ?? null;
         $this->text = $Update["message"]['text'] ?? null;
+        $this->cb_data = $Update["callback_query"]["data"] ?? null;
+        $this->cb_chat_id = $Update["callback_query"]["from"]["id"] ?? null;
+
         if(explode(" ",$this->text)[0] == "/start" and isset(explode(" ",$this->text)[1])) {
             $this->invite_id = explode(" ",$this->text)[1];
             $this->text = explode(" ",$this->text)[0];
